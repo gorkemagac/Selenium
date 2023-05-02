3b27c32dc0b0e062b3ade125f6faada763afab7e
Gorkem Agac <gorkem_agac@hotmail.com>
2023-05-03 00:01:09 +0100

    utilities

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,54 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.util.Set;
+
+public class BrowserUtils {
+
+    public static void sleep(int second)
+    {
+        second *=1000;
+        try{
+            Thread.sleep(second);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+    }
+
+    public static void switchWindowsAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle)
+    {
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+            String actualTitle = driver.getTitle();
+
+            Assert.assertTrue(actualTitle.contains(expectedInTitle));
+        }
+
+
+
+    }
+    public static void verifyTitle(WebDriver driver, String expectedTitle)
+    {
+        Assert.assertEquals(driver.getTitle(),expectedTitle);
+    }
+    public static void waitForInvisibilityOf(WebElement webElement){
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
+
+}
---
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -0,0 +1,23 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CRM_Utilities {
+
+
+    public static void crm_login(WebDriver driver, String username,String password)
+    {
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys(username);
+
+        //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys(password);
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+    }
+
+}
